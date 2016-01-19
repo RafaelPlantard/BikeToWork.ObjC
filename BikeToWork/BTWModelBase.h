d67da98f7af223cc8255e4b369cad535c3bd74d1
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-01-19 11:58:05 -0200

    Added podfile and custom model base based on bricks-Mantle.

---
+++ BikeToWork/BTWModelBase.h
@@ -0,0 +1,29 @@
+//
+//  FDCMantleBase.h
+//  FirstDribbbleChallenge
+//
+//  Created by Douglas Barreto on 1/13/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import <bricks-Mantle/BKMBaseMantleObj.h>
+
+/*!
+ @brief This class turn more easier the convert of a public property class to a json representation on mantle model.
+ @discussion 
+ It will map all public properties to a NSDictionary where the Mantle framework will get and make the bind between the properties and json properties.
+ To turn a behavior of binding class property with json property see the method customJSONKeyPathsByPropertyKey.
+ */
+@interface BTWModelBase : BKMBaseMantleObj
+
+/*! @brief The method that contains all properties that have different names on json property name side. */
++ (NSDictionary *)customJSONKeyPathsByPropertyKey;
+
+/*!
+ @brief Changes the scape that is used for separate words on property names.
+ @discussion For standard this scape is '_'.
+ @remarks This escape is used to marks the end of a break between words.
+ */
++ (NSString *)JSONScapeOnConvert;
+
+@end
---
+++ BikeToWork/BTWModelBase.m
@@ -0,0 +1,73 @@
+//
+//  FDCMantleBase.m
+//  FirstDribbbleChallenge
+//
+//  Created by Douglas Barreto on 1/13/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import "BTWModelBase.h"
+
+/*!@brief The character that marks other word in the property name on json file. */
+static NSString *const kDefaultScape = @"_";
+
+/*! @brief The pattern that is used to represent the property name with a scaped character before it. */
+static NSString *const kPatternToReplacement = @"%@$1";
+
+/*! @brief The Regular Expression that matches only letters in upper case. */
+static NSString *const kRegexPattern = @"([A-Z])";
+
+@implementation BTWModelBase
+
+#pragma mark - Static variables
+
+/*!
+ @brief The current scape character.
+ @remarks For default its value is '_'.
+ */
+static NSString *scapeCharacter;
+
+#pragma mark - Public methods
+
++ (NSDictionary *)customJSONKeyPathsByPropertyKey {
+    return [NSDictionary new];
+}
+
++ (NSString *)JSONScapeOnConvert {
+    return kDefaultScape;
+}
+
+#pragma mark - Private helpers methods
+
++ (NSDictionary *) convertDictionaryPropertiesToJsonProperties {
+    NSDictionary *mantleMap = [NSDictionary mtl_identityPropertyMapWithModel:[self class]];
+    
+    NSMutableDictionary *customMantleMap = [NSMutableDictionary new];
+    
+    [mantleMap enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        [customMantleMap setObject:[self convertPropertyClassToJSONProperty:key toEscape:[self JSONScapeOnConvert]]forKey:key];
+    }];
+    
+    [customMantleMap addEntriesFromDictionary:[self customJSONKeyPathsByPropertyKey]];
+    
+    return [NSDictionary dictionaryWithDictionary:customMantleMap];
+}
+
++ (NSString *)convertPropertyClassToJSONProperty:(NSString *)propertyName toEscape:(NSString *)escapeString {
+    NSString *replaceString = [NSString stringWithFormat:kPatternToReplacement, escapeString];
+    
+    NSError *errorsOnRegex;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:kRegexPattern options:0 error:&errorsOnRegex];
+    
+    NSString *modifiedString = [regex stringByReplacingMatchesInString:propertyName options:0 range:NSMakeRange(0, [propertyName length]) withTemplate:replaceString];
+    
+    return [modifiedString lowercaseString];
+}
+
+#pragma mark - MTLJSONSerializing
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+    return [self convertDictionaryPropertiesToJsonProperties];
+}
+
+@end
