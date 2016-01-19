700e319b343ed9eec3cb39f1e7a6c617de38f583
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-01-19 11:52:01 -0200

    Added initial session custom manager.

---
+++ BikeToWork/BTWSessionManager.h
@@ -0,0 +1,14 @@
+//
+//  BTWSessionManager.h
+//  BikeToWork
+//
+//  Created by Rafael Ferreira on 1/19/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*! @brief A custom session manager for network requests. */
+@interface BTWSessionManager : NSObject
+
+@end
